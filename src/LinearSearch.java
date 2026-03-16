00472507dfc606526664a8ff231f64314f042af7
Your Name <prateek9235kverma@gmail.com>
2026-03-16 15:00:13 +0530

    linear1

---
+++ src/LinearSearch.java
@@ -0,0 +1,14 @@
+public class LinearSearch {
+    public static void main(String[] args){
+        int[] arr= {12,34,54,67,65};
+        for(int i= 0;i<arr.length;i++){
+            if(arr[i]==67) {
+                System.out.println("number found");
+            }
+                //else
+                  //
+            //  System.out.println("number not found");
+
+        }
+    }
+}
