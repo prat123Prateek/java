bec04eb61867223f4eb844ed7221c2aa4f774dfb
Your Name <prateek9235kverma@gmail.com>
2026-03-17 21:05:20 +0530

    example

--- src/LinearSearchExample.java
+++ src/LinearSearchExample.java
@@ -1,3 +1,21 @@
 public class LinearSearchExample {
+    public static void main(String[] args){
+        String str="prateek";;
+        char target='e';
+        boolean ans=linear(str,target);
+        System.out.println(ans);
+    }
+    static boolean linear(String str ,char target){
+        for(int i=0; i<str.length(); i++){
+           // String element=str.charAt(i);
+            if(str.charAt(i)==target){
+                return true;
+            }
+
+        }
+        return false;
+
+    }
+
 
 }
