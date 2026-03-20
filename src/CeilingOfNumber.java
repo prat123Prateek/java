5cc353609354a165b5d64a6e2d22015298d3ac49
Your Name <prateek9235kverma@gmail.com>
2026-03-21 01:27:23 +0530

    ceiling

---
+++ src/CeilingOfNumber.java
@@ -0,0 +1,32 @@
+public class CeilingOfNumber {
+    public static void main(String[] args){
+        int[] arr= {4, 5, 6, 8, 10, 12, 13, 15, 17};
+        int target=14;
+        int ans=ceiling(arr, target);
+        System.out.println(ans);
+    }
+    static int ceiling(int[] arr,int target){
+        if (target > arr[arr.length -1]){
+            return -1;
+        }
+
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > target) {
+                end = mid - 1;
+            }
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+
+        }
+        return arr[start];
+    }
+}
