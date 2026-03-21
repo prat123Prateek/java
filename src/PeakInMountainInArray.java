18df66bec1371b75cc01f73c9180a4d0b4c985b5
Your Name <prateek9235kverma@gmail.com>
2026-03-22 01:20:54 +0530

    peakinmointain

---
+++ src/PeakInMountainInArray.java
@@ -0,0 +1,20 @@
+public class PeakInMountainInArray {
+    public static void main(String[] args){
+        int[] arr={1, 2, 3, 5, 7, 6 ,4, 3, 2};
+        System.out.println(PeakInArray(arr));
+    }
+    static int PeakInArray(int[] arr){
+        int start=0;
+        int end=arr.length -1;
+        while(start<end){
+            int mid =start+ (end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return arr[end];
+    }
+}
