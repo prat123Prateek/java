5820253fe674800aabe86c6c7d4b7828e3d65770
Your Name <prateek9235kverma@gmail.com>
2026-03-25 17:39:29 +0530

    cyclesort

---
+++ src/CyclicSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args){
+        int[] arr = {5,2,4,1,3,6};
+        Cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void Cyclic(int[] arr){
+        for(int i=0 ; i<arr.length;){
+            int correctindex = arr[i]-1;
+           if( arr[i] == arr[correctindex]){
+            i++;
+           }
+           else{
+               swap(arr, i, correctindex);
+           }
+
+        }
+
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
