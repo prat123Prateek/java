cc588e489bec3cefd6c0bd74a4222390696d6e93
Your Name <prateek9235kverma@gmail.com>
2026-03-31 02:51:40 +0530

    doubles

---
+++ src/FindUnique.java
@@ -0,0 +1,14 @@
+public class FindUnique {
+    public static void main(String[] args){
+        int[] arr= {1,2,3,4,3,2,1};
+        System.out.println(search(arr));
+    }
+    static int search(int[] arr){
+        int unique=0;
+        for(int i=0;i<arr.length;i++){
+            unique=unique^arr[i];
+        }
+
+        return unique;
+    }
+}
